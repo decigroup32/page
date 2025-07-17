ab7e001cc4c5b221f252cce1121606effd9f0d5d
decigroup32 <decigroup32@gmail.com>
2025-07-17 07:27:48 +0300

    ..

--- project/main.js
+++ project/main.js
@@ -1,12 +1,3 @@
-    window.addEventListener('scroll', function () {
-        const scrollY = window.scrollY;
-        const maxScroll = document.body.scrollHeight - window.innerHeight;
-        const scrollPercent = scrollY / maxScroll;
-
-        // Move from 10% to 50% left as you scroll down
-        const minLeft = 10;
-        const maxLeft = 50;
-        const newLeft = minLeft + (maxLeft - minLeft) * scrollPercent;
-
-        document.getElementById('earth').style.left = newLeft + '%';
-    });
+document.getElementById('hamburger').addEventListener('click', function () {
+  document.querySelector('.novbar .links').classList.toggle('active');
+});
